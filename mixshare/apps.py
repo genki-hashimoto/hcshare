e682f6071451b11d8ad5ba87af1f07af2a7bafe1
Genki Hashimoto <knct.gen@gmail.com>
2017-04-05 13:35:42 +0900

    app作成，Postモデル作成

---
+++ mixshare/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MixshareConfig(AppConfig):
+    name = 'mixshare'
---
+++ mixshare/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.utils import timezone
+
+class Post(models.Model):
+  url = models.TextField()
+  like = models.IntegerField()
+  play = models.IntegerField()
+  repost = models.IntegerField()
