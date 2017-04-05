b3cceeb3c25e557ad933945c6fde684dc75945ce
Genki Hashimoto <knct.gen@gmail.com>
2017-04-05 17:36:29 +0900

    表示機能は完成(デザインは後で)

--- mixshare/views.py
+++ mixshare/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
 
-# Create your views here.
+def post_list(request):
+    posts = Post.objects.filter(registed_date__lte=timezone.now()).order_by('like')
+    return render(request, 'mixshare/post_list.html', {'posts': posts})
